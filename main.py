13257468dac4a0bb57b10d71761b8c67e2c63a8a
Laura Orozco <laura.orozcoceron@mercadolibre.com>
2022-02-25 09:44:15 -0500

    finish the principal code for extraction text and convert to image

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 # pip install pdftotext
 # sudo apt install build-essential libpoppler-cpp-dev pkg-config python3-dev
 
-import pdftotext
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+
 import os
 
-path = "."
 
-cont = 0
-for root, _, file_names in os.walk(path):
-    for file_name in file_names:
-        path_file = "%s/%s" % (root, file_name)
-        with open(path_file, "rb") as f:
-            if file_name.endswith(".pdf"):
-                cont = cont + 1
-                pdf = pdftotext.PDF(f)
-                new_file = open(file_name[:-4] + ".txt", "w+")
-                new_file.write("\n\n".join(pdf))
-                new_file.close()
-print(cont)
+def create_images():
+    path = "."
+
+    cont = 0
+    for root, _, file_names in os.walk(path):
+        for file_name in file_names:
+            path_file = "%s/%s" % (root, file_name)
+            if path_file.endswith(".pdf"):
+                images = convert_from_path(path_file)
+                images[0].save("images/"+file_name[:-4]+".png")
+    print(cont)
+
+
+def images_to_text():
+    for _, _, file_names in os.walk("images"):
+        for file_name in file_names:
+            text = pytesseract.image_to_string(
+                Image.open('images/'+file_name), lang='spa')
+            new_file = open("texts/"+file_name[:-4] + ".txt", "w+")
+            new_file.write(text)
+            new_file.close()
+
+
+images_to_text()
